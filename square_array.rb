1459dafb4ac2f436469841b11e3acef10261141b
Jason J Cruz <jaycru06@yahoo.com>
2019-11-19 22:09:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   squared_numbers = []
   array.each do |number|
-    squared_numbers << number * 2 
+    number * 2 
+    squared_numbers << number 
   end 
   return squared_numbers
 end
